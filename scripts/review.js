4559c9500309d4b78aea7d9d9666707e9f7e01ad
Mozart Soares <mozartarmstrong@gmail.com>
2025-02-07 20:53:49 -0300

    feat: include form.html and review.html

---
+++ scripts/review.js
@@ -0,0 +1,14 @@
+const params = new URLSearchParams(window.location.search);
+
+document.getElementById('confirmed-product').textContent = params.get('product');
+document.getElementById('confirmed-rating').textContent = '★'.repeat(params.get('rating')) + '☆'.repeat(5 - params.get('rating'));
+document.getElementById('confirmed-date').textContent = new Date(params.get('install_date')).toLocaleDateString();
+document.getElementById('confirmed-features').textContent = params.getAll('features').join(', ') || 'None selected';
+document.getElementById('confirmed-review').textContent = params.get('review') || 'No review provided';
+document.getElementById('confirmed-user').textContent = params.get('user') || 'Anonymous';
+
+
+let reviewCount = localStorage.getItem('reviewCount') || 0;
+reviewCount = parseInt(reviewCount) + 1
+document.getElementById('review-counter').textContent = reviewCount;
+localStorage.setItem('reviewCount', reviewCount);
